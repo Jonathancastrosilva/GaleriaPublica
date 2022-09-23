7c383503cdc97a3c66dd101bd6302946c98a8ca4
20201TIIMI0101 <jonathancastrosilva8@gmail.com>
2022-09-23 08:56:53 -0300

    mandei

---
+++ app/src/main/java/castro/jonathan/galeriapblica/MainActivity.java
@@ -0,0 +1,16 @@
+package castro.jonathan.galeriapblica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import castro.jonathan.galeriapublica.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
